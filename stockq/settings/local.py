020af82aaae82e3d13b10d53095580f7ae3578a4
kyb <bigstar96115@hanmail.net>
2025-09-23 17:07:42 +0900

    1

--- stockq/settings/local.py
+++ stockq/settings/local.py
@@ -14,7 +14,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.getenv("REDIS_URL", "redis://redis:6379/1"),
         "TIMEOUT": 60 * 10,
     }
 }
