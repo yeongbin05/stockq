21bdc8d62ba01ef2ab2e59c16dea45ad49070a08
kyb <bigstar96115@hanmail.net>
2026-04-03 16:58:20 +0900

    Add dispatcher job pick log

--- stockq/settings/base.py
+++ stockq/settings/base.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     "rest_framework_simplejwt.token_blacklist",  
     'corsheaders',
-    'sandbox',
+    # 'sandbox',
     'users',
     'stocks',
     'subscriptions',
